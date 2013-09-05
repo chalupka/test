5ca5c5f8fbe6c2de1c16e8756f76ac976cb743e4
Martin.Chalupka <martin.chalupka@metrosystems.net>
2013-09-05 10:51:10 +0200

    third commit

--- GitTest/src/de/test/Test2.java
+++ GitTest/src/de/test/Test2.java
@@ -1,8 +1,10 @@
 package de.test;
 
-public class Test {
+public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		// TODO Auto-generated method stub
+
 	}
+
 }
