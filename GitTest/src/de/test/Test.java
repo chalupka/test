41af9e4f09ddfe5c3ebf03edf3c7e40c33ec8d63
Martin.Chalupka <martin.chalupka@metrosystems.net>
2013-09-09 09:59:37 +0200

    test ws 2.2

--- GitTest/src/de/test/Test.java
+++ GitTest/src/de/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Test ws1");
 		System.out.println("Hello test ws2");
+		System.out.println("Hello test ws2.2");
 		// remove this
 		// ok 
 	}
